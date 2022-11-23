ad9cb5da3a8325af2922a6aa7ce4c7bd1bb3ed3f
Jhon Calderon <jhoncalderon20174@gmail.com>
2022-11-23 15:01:00 -0500

    solo if prro

---
+++ tres.cpp
@@ -0,0 +1,40 @@
+/*
+a b c
+a c b 
+b c a
+b a c
+c a b
+c b a
+*/
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int a, b, c, aux1, aux2;
+    cout << " Ingrese 3 numeros.." <<endl;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    if (b > a){
+        aux1 = a;
+        a = b;
+        b = aux1;
+    } // en este punto a >= b, están ordenados
+    if(c > a){ //cab
+        aux1 = a;
+        aux2 = b;
+        a = c;
+        b = aux1;
+        c = aux2;
+    }else if (c < a & c >= b){//acb
+        aux1 = b;
+        b = c;
+        c = aux1;
+    }
+
+    cout << a << " " << b << " " << c;
+
+
+
+    return 0;
+}
